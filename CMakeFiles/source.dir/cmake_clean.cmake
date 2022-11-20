6cba29b45956f40a4c04b7186c540395a1dcc0b0
and1skip <and1king71@gmail.com>
2022-11-20 17:18:45 +0300

    add source and cmake files

---
+++ CMakeFiles/source.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/source.dir/source.cpp.o"
+  "source"
+  "source.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/source.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
